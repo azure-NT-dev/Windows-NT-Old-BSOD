7c968c7c44f53b3c86b0007a542c58352b3ce869
Azure <miladin34567@gmail.com>
2026-07-21 02:23:56 +0200

    Add error type classification to stop codes

--- BugCheck-Mockup.ps1
+++ BugCheck-Mockup.ps1
@@ -2,58 +2,59 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 $stopCodes = @(
-    @{ Code = "IRQL_NOT_LESS_OR_EQUAL"; Hex = "0x0000000A"; Failed = "ndis.sys" },
-    @{ Code = "APC_INDEX_MISMATCH"; Hex = "0x00000001"; Failed = "ntoskrnl.exe" },
-    @{ Code = "KMODE_EXCEPTION_NOT_HANDLED"; Hex = "0x0000001E"; Failed = "ntoskrnl.exe" },
-    @{ Code = "PAGE_FAULT_IN_NONPAGED_AREA"; Hex = "0x00000050"; Failed = "ntoskrnl.exe" },
-    @{ Code = "BAD_POOL_CALLER"; Hex = "0x000000C2"; Failed = "ndis.sys" },
-    @{ Code = "DRIVER_IRQL_NOT_LESS_OR_EQUAL"; Hex = "0x000000D1"; Failed = "nvlddmkm.sys" },
-    @{ Code = "INACCESSIBLE_BOOT_DEVICE"; Hex = "0x0000007B"; Failed = "storport.sys" },
-    @{ Code = "NTFS_FILE_SYSTEM"; Hex = "0x00000024"; Failed = "ntfs.sys" },
-    @{ Code = "PFN_LIST_CORRUPT"; Hex = "0x0000004E"; Failed = "ntoskrnl.exe" },
-    @{ Code = "SYSTEM_SERVICE_EXCEPTION"; Hex = "0x0000003B"; Failed = "win32k.sys" },
-    @{ Code = "SYSTEM_THREAD_EXCEPTION_NOT_HANDLED"; Hex = "0x0000007E"; Failed = "nvlddmkm.sys" },
-    @{ Code = "UNEXPECTED_KERNEL_MODE_TRAP"; Hex = "0x0000007F"; Failed = "ntoskrnl.exe" },
-    @{ Code = "DRIVER_OVERRAN_STACK_BUFFER"; Hex = "0x000000F7"; Failed = "ntoskrnl.exe" },
-    @{ Code = "CRITICAL_STRUCTURE_CORRUPTION"; Hex = "0x00000109"; Failed = "ntoskrnl.exe" },
-    @{ Code = "BAD_POOL_HEADER"; Hex = "0x00000019"; Failed = "ntoskrnl.exe" },
-    @{ Code = "POOL_CORRUPTION_IN_FILE_AREA"; Hex = "0x000000DE"; Failed = "ntfs.sys" },
-    @{ Code = "DRIVER_LEFT_LOCKED_PAGES_IN_PROCESS"; Hex = "0x000000CB"; Failed = "ndis.sys" },
+    @{ Code = "IRQL_NOT_LESS_OR_EQUAL"; Hex = "0x0000000A"; Failed = "ndis.sys"; Type = "Standard" },
+    @{ Code = "APC_INDEX_MISMATCH"; Hex = "0x00000001"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "KMODE_EXCEPTION_NOT_HANDLED"; Hex = "0x0000001E"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "PAGE_FAULT_IN_NONPAGED_AREA"; Hex = "0x00000050"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "BAD_POOL_CALLER"; Hex = "0x000000C2"; Failed = "ndis.sys"; Type = "Standard" },
+    @{ Code = "DRIVER_IRQL_NOT_LESS_OR_EQUAL"; Hex = "0x000000D1"; Failed = "nvlddmkm.sys"; Type = "Standard" },
+    @{ Code = "INACCESSIBLE_BOOT_DEVICE"; Hex = "0x0000007B"; Failed = "storport.sys"; Type = "Standard" },
+    @{ Code = "NTFS_FILE_SYSTEM"; Hex = "0x00000024"; Failed = "ntfs.sys"; Type = "Standard" },
+    @{ Code = "PFN_LIST_CORRUPT"; Hex = "0x0000004E"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "SYSTEM_SERVICE_EXCEPTION"; Hex = "0x0000003B"; Failed = "win32k.sys"; Type = "Standard" },
+    @{ Code = "SYSTEM_THREAD_EXCEPTION_NOT_HANDLED"; Hex = "0x0000007E"; Failed = "nvlddmkm.sys"; Type = "Standard" },
+    @{ Code = "UNEXPECTED_KERNEL_MODE_TRAP"; Hex = "0x0000007F"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "DRIVER_OVERRAN_STACK_BUFFER"; Hex = "0x000000F7"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "CRITICAL_STRUCTURE_CORRUPTION"; Hex = "0x00000109"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "BAD_POOL_HEADER"; Hex = "0x00000019"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "POOL_CORRUPTION_IN_FILE_AREA"; Hex = "0x000000DE"; Failed = "ntfs.sys"; Type = "Standard" },
+    @{ Code = "DRIVER_LEFT_LOCKED_PAGES_IN_PROCESS"; Hex = "0x000000CB"; Failed = "ndis.sys"; Type = "Standard" },
 
-    @{ Code = "DATA_BUS_ERROR"; Hex = "0x0000002E"; Failed = "" },
-    @{ Code = "NO_MORE_SYSTEM_PTES"; Hex = "0x0000003F"; Failed = "ntoskrnl.exe" },
-    @{ Code = "MISMATCHED_HAL"; Hex = "0x00000079"; Failed = "hal.dll" },
-    @{ Code = "KERNEL_STACK_INPAGE_ERROR"; Hex = "0x00000077"; Failed = "ntoskrnl.exe" },
-    @{ Code = "REGISTRY_ERROR"; Hex = "0x00000051"; Failed = "ntoskrnl.exe" },
-    @{ Code = "BAD_SYSTEM_CONFIG_INFO"; Hex = "0x00000074"; Failed = "ntoskrnl.exe" },
+    @{ Code = "DATA_BUS_ERROR"; Hex = "0x0000002E"; Failed = ""; Type = "Standard" },
+    @{ Code = "NO_MORE_SYSTEM_PTES"; Hex = "0x0000003F"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "MISMATCHED_HAL"; Hex = "0x00000079"; Failed = "hal.dll"; Type = "Standard" },
+    @{ Code = "KERNEL_STACK_INPAGE_ERROR"; Hex = "0x00000077"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "REGISTRY_ERROR"; Hex = "0x00000051"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "BAD_SYSTEM_CONFIG_INFO"; Hex = "0x00000074"; Failed = "ntoskrnl.exe"; Type = "Standard" },
 
-    @{ Code = "DRIVER_POWER_STATE_FAILURE"; Hex = "0x0000009F"; Failed = "USBPORT.SYS" },
-    @{ Code = "DRIVER_VERIFIER_DETECTED_VIOLATION"; Hex = "0x000000C4"; Failed = "verifier.sys" },
-    @{ Code = "DRIVER_CORRUPTED_EXPOOL"; Hex = "0x000000C5"; Failed = "ntoskrnl.exe" },
-    @{ Code = "DRIVER_UNLOADED_WITHOUT_CANCELLING_PENDING_OPERATIONS"; Hex = "0x000000CE"; Failed = "ntoskrnl.exe" },
-    @{ Code = "SPECIAL_POOL_DETECTED_MEMORY_CORRUPTION"; Hex = "0x000000C1"; Failed = "ntoskrnl.exe" },
+    @{ Code = "DRIVER_POWER_STATE_FAILURE"; Hex = "0x0000009F"; Failed = "USBPORT.SYS"; Type = "Standard" },
+    @{ Code = "DRIVER_VERIFIER_DETECTED_VIOLATION"; Hex = "0x000000C4"; Failed = "verifier.sys"; Type = "Standard" },
+    @{ Code = "DRIVER_CORRUPTED_EXPOOL"; Hex = "0x000000C5"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "DRIVER_UNLOADED_WITHOUT_CANCELLING_PENDING_OPERATIONS"; Hex = "0x000000CE"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "SPECIAL_POOL_DETECTED_MEMORY_CORRUPTION"; Hex = "0x000000C1"; Failed = "ntoskrnl.exe"; Type = "Standard" },
 
-    @{ Code = "THREAD_STUCK_IN_DEVICE_DRIVER"; Hex = "0x000000EA"; Failed = "nvlddmkm.sys" },
-    @{ Code = "HAL_INITIALIZATION_FAILED"; Hex = "0x0000005C"; Failed = "hal.dll" },
-    @{ Code = "MACHINE_CHECK_EXCEPTION"; Hex = "0x0000009C"; Failed = "hal.dll" },
+    @{ Code = "THREAD_STUCK_IN_DEVICE_DRIVER"; Hex = "0x000000EA"; Failed = "nvlddmkm.sys"; Type = "Standard" },
+    @{ Code = "HAL_INITIALIZATION_FAILED"; Hex = "0x0000005C"; Failed = "hal.dll"; Type = "Standard" },
+    @{ Code = "MACHINE_CHECK_EXCEPTION"; Hex = "0x0000009C"; Failed = "hal.dll"; Type = "Standard" },
     
-    @{ Code = "UNMOUNTABLE_BOOT_VOLUME"; Hex = "0x000000ED"; Failed = "" },
-    @{ Code = "KERNEL_DATA_INPAGE_ERROR"; Hex = "0x0000007A"; Failed = "disk.sys" },
-    @{ Code = "WHEA_UNCORRECTABLE_ERROR"; Hex = "0x00000124"; Failed = "" },
-    @{ Code = "CLOCK_WATCHDOG_TIMEOUT"; Hex = "0x00000101"; Failed = "intelppm.sys" },
+    @{ Code = "UNMOUNTABLE_BOOT_VOLUME"; Hex = "0x000000ED"; Failed = ""; Type = "BootFailure" },
+    @{ Code = "KERNEL_DATA_INPAGE_ERROR"; Hex = "0x0000007A"; Failed = "disk.sys"; Type = "Standard" },
+    @{ Code = "WHEA_UNCORRECTABLE_ERROR"; Hex = "0x00000124"; Failed = ""; Type = "BootFailure" },
+    @{ Code = "CLOCK_WATCHDOG_TIMEOUT"; Hex = "0x00000101"; Failed = "intelppm.sys"; Type = "Standard" },
 
-    @{ Code = "ATTEMPTED_WRITE_TO_READONLY_MEMORY"; Hex = "0x000000BE"; Failed = "ntoskrnl.exe" },
-    @{ Code = "CRITICAL_OBJECT_TERMINATION"; Hex = "0x000000F4"; Failed = "csrss.exe" },
-    @{ Code = "FAT_FILE_SYSTEM"; Hex = "0x00000023"; Failed = "fastfat.sys" },
-    @{ Code = "BUGCODE_USB_DRIVER"; Hex = "0x000000FE"; Failed = "USBPORT.SYS" },
+    @{ Code = "ATTEMPTED_WRITE_TO_READONLY_MEMORY"; Hex = "0x000000BE"; Failed = "ntoskrnl.exe"; Type = "Standard" },
+    @{ Code = "CRITICAL_OBJECT_TERMINATION"; Hex = "0x000000F4"; Failed = "csrss.exe"; Type = "UserModeTermination" },
+    @{ Code = "FAT_FILE_SYSTEM"; Hex = "0x00000023"; Failed = "fastfat.sys"; Type = "Standard" },
+    @{ Code = "BUGCODE_USB_DRIVER"; Hex = "0x000000FE"; Failed = "USBPORT.SYS"; Type = "Standard" },
 
-    @{ Code = "VIDEO_TDR_FAILURE"; Hex = "0x00000116"; Failed = "nvlddmkm.sys" }
+    @{ Code = "VIDEO_TDR_FAILURE"; Hex = "0x00000116"; Failed = "nvlddmkm.sys"; Type = "Standard" }
 )
 
 $errorChoice = $stopCodes | Get-Random
 $StopCode = $errorChoice.Code
 $hexCode = $errorChoice.Hex
 $failingFile = $errorChoice.Failed
+$errorType = $errorChoice.Type
 
 function Get-RandomKernelAddr {
     $high = (Get-Random -Min 0x1000 -Max 0xFFFF).ToString("X4")
@@ -66,7 +67,7 @@ $param2 = Get-RandomKernelAddr
 $param3 = Get-RandomKernelAddr
 $param4 = (Get-RandomKernelAddr).Replace("0x", "0X")
 
-# 1% Chance of RSOD
+# Something special.
 if ((Get-Random -Min 1 -Max 101) -le 1) {
     $screenColor = [System.Drawing.Color]::FromArgb(170, 0, 0)
 } else {
@@ -97,17 +98,29 @@ $mainFontSize = [int](20 * $scaleY)
 if ($mainFontSize -lt 9) { $mainFontSize = 9 }
 
 $classicText = "A problem has been detected and Windows has been shut down to prevent damage`r`nto your computer.`r`n`r`n"
-if ($failingFile) {
-    $classicText += "The problem seems to be caused by the following file: $failingFile`r`n`r`n"
+
+switch ($errorType) {
+    "UserModeTermination" {
+        $classicText += "A process or thread crucial to system operation has unexpectedly exited or been terminated.`r`n`r`n"
+    }
+    "BootFailure" {
+        $classicText += "$StopCode`r`n`r`n"
+    }
+    default {
+        if ($failingFile) {
+            $classicText += "The problem seems to be caused by the following file: $failingFile`r`n`r`n"
+        }
+        $classicText += "$StopCode`r`n`r`n"
+    }
 }
-$classicText += "$StopCode`r`n`r`n"
+
 $classicText += "If this is the first time you've seen this Stop error screen,`r`nrestart your computer. If this screen appears again, follow`r`nthese steps:`r`n`r`n"
 $classicText += "Check to make sure any new hardware or software is properly installed.`r`nIf this is a new installation, ask your hardware or software manufacturer`r`nfor any windows updates you might need.`r`n`r`n"
 $classicText += "If problems continue, disable or remove any newly installed hardware`r`nor software. Disable BIOS memory options such as caching or shadowing.`r`nIf you need to use safe Mode to remove or disable components, restart`r`nyour computer, press F8 to select Advanced Startup Options, and then`r`nselect safe Mode.`r`n`r`n"
 $classicText += "Technical information:`r`n`r`n"
 $classicText += "*** STOP: $hexCode ($param1,$param2,$param3,0" + $param4.Substring(1) + ")`r`n`r`n"
 
-if ($failingFile) {
+if ($errorType -ne "BootFailure" -and $failingFile) {
     $addr = (Get-RandomKernelAddr).ToLower()
     $base = (Get-RandomKernelAddr).ToLower()
     $date = "0x" + (Get-Random -Min 0x40000000 -Max 0x5FFFFFFF).ToString("X8")
@@ -148,11 +161,14 @@ $form.Add_Shown({
     $displayed += "Collecting data for crash dump ...`r`n"
     Update-Buffer $displayed
     Start-Sleep -Milliseconds 150
+    $displayed += "Initializing disk for crash dump ...`r`n"
+    Update-Buffer $displayed
+    Start-Sleep -Milliseconds 150
     $displayed += "Beginning dump of physical memory.`r`n"
     Update-Buffer $displayed
     
     $baseWithDump = $displayed + "Dumping physical memory to disk:  "
-    for ($pct = 4; $pct -le 100; $pct += (Get-Random -Min 2 -Max 7)) {
+    for ($pct = 0; $pct -le 100; $pct += (Get-Random -Min 2 -Max 7)) {
         if ($pct -gt 100) { $pct = 100 }
         Update-Buffer ($baseWithDump + $pct)
         [System.Windows.Forms.Application]::DoEvents()
