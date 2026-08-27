d07fb6c63ceb72d88bffb33027d8f54a30d6311a
Azure <miladin34567@gmail.com>
2026-08-27 09:57:26 +0200

    Correct capitalization in error message text

--- BugCheck-Mockup.ps1
+++ BugCheck-Mockup.ps1
@@ -97,7 +97,7 @@ $textYStart = [int](30 * $scaleY)
 $mainFontSize = [int](20 * $scaleY)
 if ($mainFontSize -lt 9) { $mainFontSize = 9 }
 
-$classicText = "A problem has been detected and Windows has been shut down to prevent damage`r`nto your computer.`r`n`r`n"
+$classicText = "A problem has been detected and windows has been shut down to prevent damage`r`nto your computer.`r`n`r`n"
 
 switch ($errorType) {
     "UserModeTermination" {
